786114f7b9e44cf06707bc40a6cb88b67012750a
GouravGham <105839418+GouravGham@users.noreply.github.com>
2024-06-25 23:57:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // Middleware
 app.use(cors({
-  origin: "https://milestono-web.web.app/",
+  origin: "https://milestono-web.web.app",
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
